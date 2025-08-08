a772f611b2590e7d5055030b9dffd9ef6ef3e274
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-08-08 16:21:43 +0200

    Initialization of the web interface

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Chessboard } from "react-chessboard";
+
+export default function App() {
+  return (
+    <div style={{ width: "400px" }}>
+      <Chessboard position="start" />
+    </div>
+  );
+}
